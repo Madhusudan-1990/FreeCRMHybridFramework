1dad7610fd8e615e9b412b1bf6835170dcd37ea5
Madhusudan J <madhusudanjayasimha1990@gmail.com>
2025-03-29 22:20:40 +0530

    Updating framework with cross browser parameter

--- SeleniumPOMTestNGFramework/src/main/java/com/qa/crm/factory/DriverFactory.java
+++ SeleniumPOMTestNGFramework/src/main/java/com/qa/crm/factory/DriverFactory.java
@@ -7,6 +7,7 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -36,14 +37,21 @@ public class DriverFactory
 			driver = new ChromeDriver();
 			break;
 		case "firefox":
+			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 			break;
 		case "safari":
+			WebDriverManager.safaridriver().setup();
 			driver = new SafariDriver();
 			break;
 		case "ie":
+			WebDriverManager.iedriver().setup();
 			driver = new InternetExplorerDriver();
-			break;
+			break;		
+		case "edge":
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			break;	
 			
 		default:
 			System.out.println(AppError.INVALID_BROWSER_MSG + browserName + "is invalid browser");
--- SeleniumPOMTestNGFramework/src/test/java/com/qa/crm/base/BaseTest.java
+++ SeleniumPOMTestNGFramework/src/test/java/com/qa/crm/base/BaseTest.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 
 import com.qa.crm.factory.DriverFactory;
@@ -28,12 +29,19 @@ public class BaseTest
 	protected ContactsPage contactPage;
 	
 	protected SoftAssert softAssert;
+	
+	@Parameters({"browser"})
 	@BeforeTest
-	public void setup()
+	public void setup(String browserName)
 	{
 		df = new DriverFactory();
 		prop = df.initProp();
 		
+		//Check if the browser parameter is coming from testng.xml or not.
+		if(browserName!=null)
+		{
+			prop.setProperty("browser", browserName);
+		}
 		driver = df.initDriver(prop);
 		loginPage = new LoginPage(driver);
 		softAssert = new  SoftAssert();
